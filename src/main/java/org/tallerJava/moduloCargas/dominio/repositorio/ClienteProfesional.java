6bb6d0c377e631dd55823a5b068931730b5b1746
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2026-05-14 17:29:53 -0300

    ClienteProfesional implementado

--- src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/ClienteProfesional.java
+++ src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/ClienteProfesional.java
@@ -1,6 +1,43 @@
 package org.tallerJava.moduloCargas.dominio.repositorio;
 
 public class ClienteProfesional extends Cliente{
-    //private TipoProfesional tipo;
-    private float porcentajeDescuento;
+
+    private String empresa;
+
+    private String rut;
+
+    public ClienteProfesional() {
+        super();
+    }
+
+    public ClienteProfesional(Long id,
+                              String nombre,
+                              String apellido,
+                              String email,
+                              String telefono,
+                              String empresa,
+                              String rut) {
+
+        super(id, nombre, apellido, email, telefono);
+
+        this.empresa = empresa;
+        this.rut = rut;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
 }
