efbb439ba992884147ea5aa7d585cede6df658d9
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2026-05-14 17:25:41 -0300

    Tarjeta implementado y medio de pago corregido

--- src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/MedioPago.java
+++ src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/MedioPago.java
@@ -3,16 +3,12 @@ package org.tallerJava.moduloCargas.dominio.repositorio;
 public abstract class MedioPago {
 
     private Long id;
-    private String tipo;
-    private String numero;
 
     public MedioPago() {
     }
 
-    public MedioPago(Long id, String tipo, String numero) {
+    public MedioPago(Long id) {
         this.id = id;
-        this.tipo = tipo;
-        this.numero = numero;
     }
 
     public Long getId() {
@@ -21,21 +17,5 @@ public abstract class MedioPago {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
     }
 }
--- src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/Tarjeta.java
+++ src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/Tarjeta.java
@@ -4,9 +4,69 @@ import java.time.LocalDate;
 
 public class Tarjeta extends MedioPago{
     private String numero;
-    private LocalDate fechaVencimiento;
-    private String digitoVerificacion;
-    //private TipoTarjeta tipo;
 
+    private String titular;
+
+    private String vencimiento;
+
+    private String codigoSeguridad;
+
+    private String marca;
+
+    public Tarjeta(Long id,
+                   String numero,
+                   String titular,
+                   String vencimiento,
+                   String codigoSeguridad,
+                   String marca) {
+
+        super(id);
+
+        this.numero = numero;
+        this.titular = titular;
+        this.vencimiento = vencimiento;
+        this.codigoSeguridad = codigoSeguridad;
+        this.marca = marca;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(String vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+    public String getCodigoSeguridad() {
+        return codigoSeguridad;
+    }
+
+    public void setCodigoSeguridad(String codigoSeguridad) {
+        this.codigoSeguridad = codigoSeguridad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 
 }
