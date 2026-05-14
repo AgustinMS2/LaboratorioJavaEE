599a8c96984d6874a855e9322bc25780ba35d063
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2026-05-14 17:17:47 -0300

    Carga implementado

--- src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/Carga.java
+++ src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/Carga.java
@@ -1,15 +1,93 @@
 package org.tallerJava.moduloCargas.dominio.repositorio;
 
+import org.tallerJava.moduloCargas.dominio.repositorio.Cliente;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Carga {
-    private LocalDate fecha;
-    private LocalDateTime horaInicio;
-    private LocalDateTime horaFin;
-    private float importeTotal;
-    private float recargoPorDemora;
-    private int porcentajeAvance;
-    private LocalDateTime horaEstimadaFin;
-    //private EstadoCarga estado;
+    private Long id;
+
+    private Cliente cliente;
+
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+
+    private Double consumoKwh;
+
+    private Double importe;
+
+    private Boolean finalizada;
+
+    public Carga() {
+    }
+
+    public Carga(Long id, Cliente cliente, LocalDateTime fechaInicio) {
+        this.id = id;
+        this.cliente = cliente;
+        this.fechaInicio = fechaInicio;
+        this.finalizada = false;
+    }
+
+    public void finalizar(Double consumoKwh, Double importe) {
+        this.consumoKwh = consumoKwh;
+        this.importe = importe;
+        this.fechaFin = LocalDateTime.now();
+        this.finalizada = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDateTime getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDateTime fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDateTime getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDateTime fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Double getConsumoKwh() {
+        return consumoKwh;
+    }
+
+    public void setConsumoKwh(Double consumoKwh) {
+        this.consumoKwh = consumoKwh;
+    }
+
+    public Double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(Double importe) {
+        this.importe = importe;
+    }
+
+    public Boolean getFinalizada() {
+        return finalizada;
+    }
+
+    public void setFinalizada(Boolean finalizada) {
+        this.finalizada = finalizada;
+    }
 }
