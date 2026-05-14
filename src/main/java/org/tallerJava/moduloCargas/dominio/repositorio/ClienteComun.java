25a5b8ed4e15a63e01a772b3f32abc0eb9719e8d
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2026-05-14 17:29:09 -0300

    ClienteComun implementado

--- src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/ClienteComun.java
+++ src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/ClienteComun.java
@@ -1,4 +1,16 @@
 package org.tallerJava.moduloCargas.dominio.repositorio;
 
 public class ClienteComun extends Cliente{
+    public ClienteComun() {
+        super();
+    }
+
+    public ClienteComun(Long id,
+                        String nombre,
+                        String apellido,
+                        String email,
+                        String telefono) {
+
+        super(id, nombre, apellido, email, telefono);
+    }
 }
