2b5302c4cd95bde428dd2dbe766f68e380507a3b
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2026-05-14 17:30:50 -0300

    CuentaUTE implementado

--- src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/CuentaUTE.java
+++ src/main/java/org/tallerJava/moduloCargas/dominio/repositorio/CuentaUTE.java
@@ -2,4 +2,48 @@ package org.tallerJava.moduloCargas.dominio.repositorio;
 
 public class CuentaUTE extends MedioPago{
     private String numeroCuenta;
+
+    private String titular;
+
+    private String direccionServicio;
+
+    public CuentaUTE() {
+        super();
+    }
+
+    public CuentaUTE(Long id,
+                     String numeroCuenta,
+                     String titular,
+                     String direccionServicio) {
+
+        super(id);
+
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.direccionServicio = direccionServicio;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getDireccionServicio() {
+        return direccionServicio;
+    }
+
+    public void setDireccionServicio(String direccionServicio) {
+        this.direccionServicio = direccionServicio;
+    }
 }
